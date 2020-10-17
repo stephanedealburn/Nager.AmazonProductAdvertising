2bae4798ca249996e0d568b9e93078d7c10074c2
Stéphane <stephane@dealburn.fr>
2020-10-17 08:17:45 +0200

    ReleaseDate is sometimes wrongly formed, by example "2018T"

--- src/Nager.AmazonProductAdvertising/Model/Paapi/ProductInfo.cs
+++ src/Nager.AmazonProductAdvertising/Model/Paapi/ProductInfo.cs
@@ -7,7 +7,7 @@ namespace Nager.AmazonProductAdvertising.Model.Paapi
         public DisplayValueItem<string> Color { get; set; }
         public ItemDimensions ItemDimensions { get; set; }
         public DisplayValueItem<int> UnitCount { get; set; }
-        public DisplayValueItem<DateTime> ReleaseDate { get; set; }
+        //public DisplayValueItem<DateTime> ReleaseDate { get; set; }
         public DisplayValueItem<string> Size { get; set; }
         public DisplayValueItem<bool> IsAdultProduct { get; set; }
     }
